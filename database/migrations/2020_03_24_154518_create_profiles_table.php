2e046e4c075ae3ef3b969a5b78aec4b1f8092815
sumanahmed <suman777333@gmail.com>
2020-03-24 22:01:16 +0600

    profiles migration added

---
+++ database/migrations/2020_03_24_154518_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('user_referrel_amount',10,2)->default(0);
+            $table->decimal('driver_referrel_amount',10,2)->default(0);
+            $table->string('logo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
