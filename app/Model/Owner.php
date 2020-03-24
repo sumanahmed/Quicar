f06cd8c1585bc38b8c44c339e3c29c5811fac8b9
sumanahmed <suman777333@gmail.com>
2020-03-24 17:24:34 +0600

    update for laravel package passport installed

--- app/Model/Owner.php
+++ app/Model/Owner.php
@@ -2,9 +2,15 @@
 
 namespace App\Model;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class Owner extends Model
 {
-    //
+    use HasApiTokens,Notifiable;
+    protected $fillable = [
+        'name', 'email', 'phone',
+    ];
 }
