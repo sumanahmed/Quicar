dfade866a9377547e28783e0f2f8d89992fdc901
sumanahmed <suman777333@gmail.com>
2020-03-25 11:55:43 +0600

    route added for owner login

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::post('user_register','Api\AuthController@userRegister');
 Route::post('user_login','Api\AuthController@userLogin');
+Route::post('owner_register','Api\AuthController@ownerRegister');
+Route::post('owner_login','Api\AuthController@ownerLogin');
 
 Route::group(['prefix'=>'user', 'middleware'=>'api_user_auth' ],function(){
     Route::get('/logout', 'Api\UserController@logout');
