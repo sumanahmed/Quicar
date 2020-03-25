af03507d9ee971cebe9453570a986e1ec335dac1
sumanahmed <suman777333@gmail.com>
2020-03-25 10:35:19 +0600

    route added for user logged out

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,7 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::post('user_register','Api\AuthController@userRegister');
 Route::post('user_login','Api\AuthController@userLogin');
+
+Route::group(['prefix'=>'user', 'middleware'=>'api_user_auth' ],function(){
+    Route::get('/logout', 'Api\UserController@logout');
+});
