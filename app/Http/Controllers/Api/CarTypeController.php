595f1cec64a9325d1053e960070a79496288d13b
sumanahmed <suman777333@gmail.com>
2020-03-28 14:15:07 +0600

    method added to get all car types

--- app/Http/Controllers/Api/CarTypeController.php
+++ app/Http/Controllers/Api/CarTypeController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Model\CarType;
 use Illuminate\Http\Request;
 
 class CarTypeController extends Controller
 {
-    //
+    //get all car types
+    public function getCarTypes(){
+        $car_types = CarType::all();
+        if($car_types->count() > 0){
+            return response()->json([
+                'status'    => 'success',
+                'data'      => $car_types
+            ],200);
+        }else{
+            return response()->json([
+                'status'    => 'error',
+                'data'      => []
+            ],404);
+        }
+    }
 }
