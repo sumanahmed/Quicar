7b22443b9dd1f54144bbdb3f4ceb09f8ea7bf289
sumanahmed <suman777333@gmail.com>
2020-03-29 15:58:54 +0600

    method added for get all packages

--- app/Http/Controllers/Api/PackageController.php
+++ app/Http/Controllers/Api/PackageController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Model\Package;
 use Illuminate\Http\Request;
 
 class PackageController extends Controller
 {
-    //
+    //get all packages
+    public function index(Request $request){
+        if($request->sort != null){
+            $sort = $request->sort;
+        }else{
+            $sort = "desc";
+        }
+        $packages = Package::orderBy('id', $sort)->get();
+        if($packages != null){
+            return response([
+                'status' => 'error',
+                'data'   => $packages
+            ], 200);
+        }else{
+            return response([
+                'status' => 'error',
+                'data'   => []
+            ], 404);
+        }
+    }
 }
