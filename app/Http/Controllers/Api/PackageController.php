aae452f9e2f41f024a8e5584051c09fba08ca897
sumanahmed <suman777333@gmail.com>
2020-03-29 17:27:26 +0600

    method added to package store

--- app/Http/Controllers/Api/PackageController.php
+++ app/Http/Controllers/Api/PackageController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Package\PackageResource;
+use App\Model\Owner;
 use App\Model\Package;
 use Illuminate\Http\Request;
+use Validator;
 
 class PackageController extends Controller
 {
@@ -28,4 +31,44 @@ class PackageController extends Controller
             ], 404);
         }
     }
+
+    //package store
+    public function store(Request $request){
+        $owner = Owner::select('id')->where('owner_key', $request->api_token)->first();
+        $validator=Validator::make($request->all(),[
+            'car_id'  => 'required',
+            'name'    => 'required',
+            'price'   => 'required',
+            'type'    => 'required',
+            'start'   => 'required',
+            'end'     => 'required',
+        ]);
+        if($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'data'   => 'Sorry, required column missing'
+            ],403);
+        }else{
+            $package            = new Package();
+            $package->owner_id  = $owner->id;
+            $package->car_id    = $request->car_id;
+            $package->name      = $request->name;
+            $package->price     = $request->price;
+            $package->type      = $request->type;
+            $package->start     = date('Y-m-d H:i:s', strtotime($request->start));
+            $package->end       = date('Y-m-d H:i:s', strtotime($request->end));
+            if($package->save()){
+                return response([
+                    'status' => 'success',
+                    'data'   => new PackageResource($package)
+                ],201);
+            }else{
+                return response([
+                    'status' => 'error',
+                    'data'   => []
+                ], 403);
+            }
+        }
+    }
+
 }
