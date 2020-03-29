78ce91ebeb2b00aa7f452c958a72e639ad38ff24
sumanahmed <suman777333@gmail.com>
2020-03-29 15:29:59 +0600

    detail method modify for owner detail resource

--- app/Http/Controllers/Api/OwnerController.php
+++ app/Http/Controllers/Api/OwnerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Owner\OwnerResource;
 use App\Model\Owner;
 use Illuminate\Http\Request;
 
@@ -32,8 +33,8 @@ class OwnerController extends Controller
         $owner = Owner::where('owner_key', $request->api_token)->first();
         if($owner != null){
             return response([
-                'status' => 'error',
-                'data'   => $owner
+                'status' => 'success',
+                'data'   => new OwnerResource($owner)
             ], 200);
         }else{
             return response([
