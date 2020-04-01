b50977a805066e466fb634d44ac65a9b080910e1
sumanahmed <suman777333@gmail.com>
2020-04-01 08:20:29 +0600

    unnecessary code remove

--- resources/views/quicar/backend/includes/footer.blade.php
+++ resources/views/quicar/backend/includes/footer.blade.php
@@ -7,10 +7,6 @@
 <script src="{{ asset('quicar/backend/js/dashforge.aside.js')}}"></script>
 <script src="{{ asset('quicar/backend/js/toastr.js')}}"></script>
 <script src="{{ asset('quicar/frontend/js/jquery.datetimepicker.full.min.js') }}"></script>
-<script src="{{ asset('quicar/backend/js/admin.js')}}"></script>
-<script src="{{ asset('quicar/backend/js/quote.js')}}"></script>
-<script src="{{ asset('quicar/backend/js/category.js')}}"></script>
-<script src="{{ asset('quicar/backend/js/custom.js')}}"></script>
 <script src="{{ asset('quicar/backend/js/jquery.dataTables.min.js')}}"></script>
 <script src="{{ asset('quicar/backend/js/dataTables.responsive.min.js')}}"></script>
 <script src="{{ asset('quicar/backend/js/responsive.dataTables.min.js')}}"></script>
