a8533c143e36a7e6064eeab519cd7f27986cca10
sumanahmed <suman777333@gmail.com>
2020-04-02 07:53:19 +0600

    eloquent relationship create with owner & car

--- app/Model/Package.php
+++ app/Model/Package.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Package extends Model
 {
-    //
+    //get owner
+    public function owner(){
+        return $this->belongsTo('App\Model\Owner', 'owner_id');
+    }
+    //get car
+    public function car(){
+        return $this->belongsTo('App\Model\Car', 'car_id');
+    }
 }
