5d3779aaad2c22f1f3624dc7511296bc7dfe762d
sumanahmed <suman777333@gmail.com>
2020-04-02 07:53:35 +0600

    menu added for manage package

--- resources/views/quicar/backend/includes/menu.blade.php
+++ resources/views/quicar/backend/includes/menu.blade.php
@@ -11,6 +11,7 @@
             <li id="dashboard" class="nav-item"><a href="{{ route('backend.dashboard') }}" class="nav-link"><i data-feather="database"></i> <span>Dashboard</span></a></li>
             <li id="car_type" class="nav-item"><a href="{{ route('backend.car_types') }}" class="nav-link"><i data-feather="share-2"></i> <span>Car Type</span></a></li>
             <li id="banner" class="nav-item"><a href="{{ route('backend.banner') }}" class="nav-link"><i data-feather="share-2"></i> <span>Banner</span></a></li>
+            <li id="package" class="nav-item"><a href="{{ route('backend.package') }}" class="nav-link"><i data-feather="share-2"></i> <span>Package</span></a></li>
         </ul>
     </div>
 </aside>
