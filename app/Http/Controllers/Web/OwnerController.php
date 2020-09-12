94e3fa31b19f11f06276c2e58189cad2cfc8426f
sumanahmed <suman777333@gmail.com>
2020-09-12 23:33:01 +0600

    method added to owner status update

--- app/Http/Controllers/Web/OwnerController.php
+++ app/Http/Controllers/Web/OwnerController.php
@@ -13,4 +13,16 @@ class OwnerController extends Controller
         $owners = Owner::all();
         return view('quicar.backend.owner.index', compact('owners'));
     }
+
+    //status update
+    public function statusUpdate(Request $request){ 
+        $owner                   = Owner::find($request->owner_id);
+        $owner->account_status   = $request->status;
+        $owner->update();
+        if($owner->update()){
+            return redirect()->back()->with('message', 'Status update successfully');
+        }else{
+            return redirect()->back()->with('error_message', 'Sorry something went wrong');
+        }
+    }
 }
