74dfad324266a955aeaee1f20efc8bdb61f5845b
sumanahmed <suman777333@gmail.com>
2020-09-12 23:10:05 +0600

    user js added for manage user

---
+++ public/quicar/backend/js/user.js
@@ -0,0 +1,8 @@
+$('#userTable').DataTable({
+    responsive: true,
+    language: {
+        searchPlaceholder: 'Search...',
+        sSearch: '',
+        lengthMenu: '_MENU_ items/page',
+    }
+});
