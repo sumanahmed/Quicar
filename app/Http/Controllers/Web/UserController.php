303593ee391f4bf42fc03a561725b6ea2faab521
sumanahmed <suman777333@gmail.com>
2020-09-12 23:11:00 +0600

    mehtod added for user status update

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -8,9 +8,21 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //show all owners
+    //show all users
     public function index(){
         $users = User::all();
         return view('quicar.backend.user.index', compact('users'));
+    }
+
+    //status update
+    public function statusUpdate(Request $request){ 
+        $user                   = User::find($request->user_id);
+        $user->account_status   = $request->status;
+        $user->update();
+        if($user->update()){
+            return redirect()->back()->with('message', 'Status update successfully');
+        }else{
+            return redirect()->back()->with('error_message', 'Sorry something went wrong');
+        }
     }
 }
