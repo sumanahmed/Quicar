7fdd1b512f03744779fd8fa005c1ce32e36242ab
sumanahmed <suman777333@gmail.com>
2020-09-14 08:22:45 +0600

    index method modify to get driver details

--- app/Http/Controllers/Web/CarController.php
+++ app/Http/Controllers/Web/CarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Model\Car;
+use App\Model\CarYear;
 use App\Model\CarModel;
 use App\Model\CarBrand;
 use App\Model\CarClass;
@@ -16,8 +17,11 @@ class CarController extends Controller
     //show all cars
     public function index(){
         $cars = Car::join('owners','owners.api_token','cars.owner_id')
+                    ->join('drivers','drivers.id','cars.driver_id')
                     ->select('cars.id','cars.name','cars.img1','cars.c_status','owners.name as owner_name',
-                            'owners.phone as owner_phone')
+                            'owners.phone as owner_phone','drivers.name as driver_name','drivers.phone as driver_phone',
+                            'drivers.c_status as driver_current_status','drivers.license','drivers.nid','drivers.account_status'
+                            )
                     ->get();
         return view('quicar.backend.car.index', compact('cars'));
     }
@@ -25,11 +29,12 @@ class CarController extends Controller
     //show edit page
     public function edit($id){
         $car        = Car::find($id);
+        $years      = CarYear::all();
         $models     = CarModel::all();
         $brands     = CarBrand::all();
         $classes    = CarClass::all();
         $colors     = CarColor::all();
         $districts  = CarDistrict::all();
-        return view('quicar.backend.car.edit', compact('car'));
+        return view('quicar.backend.car.edit', compact('car','years','models','brands','classes','colors','districts'));
     }
 }
