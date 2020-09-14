08c85d74a703c8f278ac784adb9984c30443bd14
sumanahmed <suman777333@gmail.com>
2020-09-14 08:23:34 +0600

    car edit blade car info shows without image done

--- resources/views/quicar/backend/car/edit.blade.php
+++ resources/views/quicar/backend/car/edit.blade.php
@@ -5,23 +5,134 @@
         <div class="container-fluid pd-x-0">
             <div class="row row-xs">
                 <div class="col-sm-12 col-lg-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="mt-2 tx-spacing--1 float-left">Update Car</h4>
-                        </div>
-                        <div class="card-body">
-                            <form action="" method="post" enctype="multipart/form-data">
-                                <div class="form-group mg-b-5">
-                                    <label>Name: <span class="tx-danger">*</span></label>
-                                    <input type="text" name="name" class="form-control wd-250 parsley-error" placeholder="Enter firstname">
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="mt-2 tx-spacing--1 float-left">Update Car</h4>
+                            </div>
+                            <div class="card-body">                                
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="name">Name <span class="text-danger" title="Required">*</span></label>
+                                            <input type="text" id="name" name="name" value="{{ $car->name }}" class="form-control" required>
+                                            <span class="text-danger errorName"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">                                        <div class="form-group">
+                                            <label for="registration_no"> Registration No <span class="text-danger" title="Required">*</span></label>
+                                            <input type="text" id="registration_no" name="registration_no" value="{{ $car->registration_no }}" class="form-control" required>
+                                            <span class="text-danger errorRegistrationNo"></span>
+                                        </div>
+                                    </div>
+                                </div>                                
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <label for="year">Car Year <span class="text-danger" title="Required">*</span></label>                                            
+                                            <select id="year" name="year" class="form-control selectable" required>
+                                                @foreach($years as $year)
+                                                    <option value="{{ $year->value }}" @if($year->value == $car->year) selected @endif>{{ $year->value }}</option>
+                                                @endforeach
+                                            </select>
+                                            <span class="text-danger errorYear"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <label for="model">Car Model <span class="text-danger" title="Required">*</span></label>                                            
+                                            <select id="model" name="model" class="form-control selectable" required>
+                                                @foreach($models as $model)
+                                                    <option value="{{ $model->value }}" @if($model->value == $car->model) selected @endif>{{ $model->value }}</option>
+                                                @endforeach
+                                            </select>
+                                            <span class="text-danger errorModel"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <label for="brand">Car Brand <span class="text-danger" title="Required">*</span></label>                                            
+                                            <select id="brand" name="brand" class="form-control selectable" required>
+                                                @foreach($brands as $brand)
+                                                    <option value="{{ $brand->value }}" @if($brand->value == $car->brand) selected @endif>{{ $brand->value }}</option>
+                                                @endforeach
+                                            </select>
+                                            <span class="text-danger errorYear"></span>
+                                        </div>
+                                    </div>
+                                </div>                                
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <label for="car_class">Car Class <span class="text-danger" title="Required">*</span></label>                                            
+                                            <select id="car_class" name="car_class" class="form-control selectable" required>
+                                                @foreach($classes as $class)
+                                                    <option value="{{ $class->value }}" @if($class->value == $car->car_class) selected @endif>{{ $class->value }}</option>
+                                                @endforeach
+                                            </select>
+                                            <span class="text-danger errorClass"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <label for="color">Car Color <span class="text-danger" title="Required">*</span></label>                                            
+                                            <select id="color" name="color" class="form-control selectable" required>
+                                                @foreach($colors as $color)
+                                                    <option value="{{ $color->value }}" @if($color->value == $car->color) selected @endif>{{ $color->value }}</option>
+                                                @endforeach
+                                            </select>
+                                            <span class="text-danger errorColor"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <!-- <label for="brand">Car Brand <span class="text-danger" title="Required">*</span></label>                                            
+                                            <select id="brand" name="brand" class="form-control" required>
+                                                @foreach($brands as $brand)
+                                                    <option value="{{ $brand->value }}" @if($brand->value == $car->brand) selected @endif>{{ $brand->value }}</option>
+                                                @endforeach
+                                            </select>
+                                            <span class="text-danger errorYear"></span> -->
+                                        </div>
+                                    </div>
+                                </div>                                
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <label for="capacity">Capacity <span class="text-danger" title="Required">*</span></label>                                            
+                                            <input type="text" id="capacity" name="capacity" class="form-control" value="{{ $car->capacity }}" required />
+                                            <span class="text-danger errorCapacity"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <label for="tax">Tax </label>                                            
+                                            <input type="text" id="tax" name="tax" class="form-control" value="{{ $car->tax }}"/>
+                                            <span class="text-danger errorTax"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <label for="tax">Fitness </label>                                            
+                                            <input type="text" id="fitness" name="fitness" class="form-control" value="{{ $car->fitness }}" />
+                                            <span class="text-danger errorFitness"></span>
+                                        </div>
+                                    </div>
+                                </div> 
+                                
+                                
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="form-group">
+                                            <input type="submit" class="btn btn-success" value="Update"/>
+                                            <input type="reset" class="btn btn-danger" value="Cancel"/>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="form-group mg-b-0">
-                                    <label>Name: <span class="tx-danger">*</span></label>
-                                    <input type="text" name="name" class="form-control wd-250 parsley-error" placeholder="Enter firstname">
-                                </div>
-                            </form>
+
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div><!-- col -->
             </div><!-- row -->
         </div><!-- container -->
