0e78a0a9bd4330f65792dacac83012b857d05c48
sumanahmed <suman777333@gmail.com>
2020-09-22 08:07:59 +0600

    method to feedback reply & delete

--- app/Http/Controllers/Web/FeedbackController.php
+++ app/Http/Controllers/Web/FeedbackController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\Feedback;
+use Validator;
+use Response;
 
 class FeedbackController extends Controller
 {
@@ -12,5 +14,35 @@ class FeedbackController extends Controller
     public function index(){
         $feedbacks = Feedback::all();
         return view('quicar.backend.feedback.index', compact('feedbacks'));
+    }
+
+    //feedback reply
+    public function reply(Request $request){
+        $validators=Validator::make($request->all(),[
+            'reply' => 'required',
+        ]);
+        if($validators->fails()){
+            return Response::json(['errors'=>$validators->getMessageBag()->toArray()]);
+        }else{
+            $feedback          = Feedback::find($request->id);
+            $feedback->replay   = $request->reply;
+            if($feedback->update()){
+                return Response::json([
+                    'status'    => 201,
+                    'data'      => $feedback
+                ]);
+            }else{
+                return Response::json([
+                    'status'        => 403,
+                    'data'          => []
+                ]);
+            }
+        }
+    }
+
+    //feedback destroy
+    public function destroy(Request $request){
+        $feedback = Feedback::find($request->id)->delete();
+        return response()->json();
     }
 }
