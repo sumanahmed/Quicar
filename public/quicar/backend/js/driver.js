18af18ca92d7f8e32ac5a7f89ad6cca5b5d45ebd
sumanahmed <suman777333@gmail.com>
2020-09-22 08:38:08 +0600

    driver js added to manage driver

---
+++ public/quicar/backend/js/driver.js
@@ -0,0 +1,8 @@
+$('#driverTable').DataTable({
+    responsive: true,
+    language: {
+        searchPlaceholder: 'Search...',
+        sSearch: '',
+        lengthMenu: '_MENU_ items/page',
+    }
+});
