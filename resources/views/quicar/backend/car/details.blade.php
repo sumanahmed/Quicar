82ad44eafb08cef6610e8f4f250838a109786cca
sumanahmed <suman777333@gmail.com>
2020-09-27 23:12:24 +0600

    car details page driver details info shows

--- resources/views/quicar/backend/car/details.blade.php
+++ resources/views/quicar/backend/car/details.blade.php
@@ -13,14 +13,14 @@
                             <div class="row">
                                 <div class="col-6">
                                     <div class="form-group">
-                                        <label for="name">Name <span class="text-danger" title="Required">*</span></label>
+                                        <label for="name">Name</label>
                                         <input type="text" id="name" name="name" value="{{ $car->name }}" class="form-control" readonly>
                                         <span class="text-danger errorName"></span>
                                     </div>
                                 </div>
                                 <div class="col-6">                                        
                                     <div class="form-group">
-                                        <label for="registration_no"> Registration No <span class="text-danger" title="Required">*</span></label>
+                                        <label for="registration_no"> Registration No</label>
                                         <input type="text" id="registration_no" name="registration_no" value="{{ $car->registration_no }}" class="form-control" readonly>
                                         <span class="text-danger errorRegistrationNo"></span>
                                     </div>
@@ -29,19 +29,19 @@
                             <div class="row">
                                 <div class="col-4">
                                     <div class="form-group">
-                                        <label for="year">Car Year <span class="text-danger" title="Required">*</span></label>                                            
+                                        <label for="year">Car Year</label>                                            
                                         <input type="text" value="{{ $car->year }}" class="form-control" readonly>
                                     </div>
                                 </div>
                                 <div class="col-4">
                                     <div class="form-group">
-                                        <label for="model">Car Model <span class="text-danger" title="Required">*</span></label>                                            
+                                        <label for="model">Car Model</label>                                            
                                         <input type="text" value="{{ $car->model }}" class="form-control" readonly>
                                     </div>
                                 </div>
                                 <div class="col-4">
                                     <div class="form-group">
-                                        <label for="brand">Car Brand <span class="text-danger" title="Required">*</span></label>                                            
+                                        <label for="brand">Car Brand</label>                                            
                                         <input type="text" value="{{ $car->brand }}" class="form-control" readonly>
                                     </div>
                                 </div>
@@ -49,13 +49,13 @@
                             <div class="row">
                                 <div class="col-4">
                                     <div class="form-group">
-                                        <label for="car_class">Car Class <span class="text-danger" title="Required">*</span></label>                                            
+                                        <label for="car_class">Car Class</label>                                            
                                         <input type="text" value="{{ $car->car_class }}" class="form-control" readonly>
                                     </div>
                                 </div>
                                 <div class="col-4">
                                     <div class="form-group">
-                                        <label for="color">Car Color <span class="text-danger" title="Required">*</span></label>                                            
+                                        <label for="color">Car Color</label>                                            
                                         <input type="text" value="{{ $car->color }}" class="form-control" readonly>
                                     </div>
                                 </div>
@@ -68,7 +68,7 @@
                             <div class="row">
                                 <div class="col-4">
                                     <div class="form-group">
-                                        <label for="capacity">Capacity <span class="text-danger" title="Required">*</span></label>                                            
+                                        <label for="capacity">Capacity</label>                                            
                                         <input type="text" class="form-control" value="{{ $car->capacity }}" readonly />
                                         <span class="text-danger errorCapacity"></span>
                                     </div>
@@ -88,38 +88,59 @@
                                     </div>
                                 </div>
                             </div> 
-                            
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="card">
-                                        <div class="card-header">Driver Details</div>
-                                        <div class="card-body">
-                                            <div class="row">
-                                                <div class="col-6">
-                                                    <div class="form-group">
-                                                        <label for="img1">Image One <span class="text-danger" title="Required">*</span></label>
-                                                        <div class="avatar-upload">
-                                                            <div class="avatar-edit">
-                                                                <input type='file' name="img1" id="img1Upload" accept=".png, .jpg, .jpeg" required/>
-                                                                <label for="img1Upload"><i class="fas fa-pencil-alt"></i></label>
-                                                            </div>
-                                                            <div class="avatar-preview" style="width:100%">
-                                                                <div id="img1Preview" style="background-image: url({{ asset('quicar/backend/img/upload2.jpg') }});"></div>
-                                                            </div>
+
+                            @php 
+                                if($car->driver_id != 0){
+                                    $driver = \App\Model\Driver::find($car->driver_id);
+                                }                                
+                            @endphp
+
+                            @if($car->driver_id != 0)
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="card">
+                                            <div class="card-header">Driver Details</div>
+                                            <div class="card-body">
+                                                <div class="row">
+                                                    <div class="col-4">
+                                                        <div class="form-group">
+                                                            <label for="capacity">Name</label>                                            
+                                                            <input type="text" class="form-control" value="{{ $driver->name }}" readonly />
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <div class="form-group">
+                                                            <label for="tax">Phone </label>                                            
+                                                            <input type="text" name="tax" class="form-control" value="{{ $driver->phone }}" readonly/>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <div class="form-group">
+                                                            <label for="tax">Current Status </label>                                            
+                                                            <input type="text" class="form-control" value="{{ $driver->c_status == 0 ? 'Off Ride' : 'On Ride' }}" readonly />
+                                                            <span class="text-danger errorFitness"></span>
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div class="col-6">
-                                                    <div class="form-group">
-                                                        <label for="img2">Image Two </label>
-                                                        <div class="avatar-upload">
-                                                            <div class="avatar-edit">
-                                                                <input type='file' name="img2" id="img2Upload" accept=".png, .jpg, .jpeg"/>
-                                                                <label for="img2Upload"><i class="fas fa-pencil-alt"></i></label>
-                                                            </div>
-                                                            <div class="avatar-preview" style="width:100%">
-                                                                <div id="img2Preview" style="background-image: url({{ asset('quicar/backend/img/upload2.jpg') }});"></div>
-                                                            </div>
+                                                <div class="row">
+                                                    <div class="col-6">
+                                                        <div class="form-group">
+                                                            <label for="capacity">Image</label>                                            
+                                                            <div class="avatar-upload">
+                                                                <div class="avatar-preview" style="width:100%">
+                                                                    <div id="img1Preview" style="background-image: url({{ asset($driver->image) }});"></div>
+                                                                </div>
+                                                            </div>                                                            
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <div class="form-group">
+                                                            <label for="capacity">License</label>                                            
+                                                            <div class="avatar-upload">
+                                                                <div class="avatar-preview" style="width:100%">
+                                                                    <div id="img1Preview" style="background-image: url({{ asset($driver->license) }});"></div>
+                                                                </div>
+                                                            </div>                                                            
                                                         </div>
                                                     </div>
                                                 </div>
@@ -127,9 +148,9 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            @endif
 
-                            <div class="row">
+                            <div class="row mt-4">
                                 <div class="col-12">
                                     <div class="card">
                                         <div class="card-header">Current Ride Details</div>
@@ -137,7 +158,7 @@
                                             <div class="row">
                                                 <div class="col-6">
                                                     <div class="form-group">
-                                                        <label for="img1">Image One <span class="text-danger" title="Required">*</span></label>
+                                                        <label for="img1">Image One</label>
                                                         <div class="avatar-upload">
                                                             <div class="avatar-edit">
                                                                 <input type='file' name="img1" id="img1Upload" accept=".png, .jpg, .jpeg" required/>
@@ -177,7 +198,7 @@
                                             <div class="row">
                                                 <div class="col-6">
                                                     <div class="form-group">
-                                                        <label for="img1">Image One <span class="text-danger" title="Required">*</span></label>
+                                                        <label for="img1">Image One</label>
                                                         <div class="avatar-upload">
                                                             <div class="avatar-edit">
                                                                 <input type='file' name="img1" id="img1Upload" accept=".png, .jpg, .jpeg" required/>
