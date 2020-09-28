b0353e0a4efc7ead026fa4f5a025795576f8729d
sumanahmed <suman777333@gmail.com>
2020-09-28 22:13:20 +0600

    menu selected class added

--- resources/views/quicar/backend/ride/package_ride.blade.php
+++ resources/views/quicar/backend/ride/package_ride.blade.php
@@ -57,6 +57,7 @@
 @section('scripts')
 <script src="{{ asset('quicar/backend/js/ride.js')}}"></script>
     <script>
+        $(".menu-order-dropdown").addClass('show');
         $("#pakcage_ride").addClass('active');
     </script>
 @endsection
