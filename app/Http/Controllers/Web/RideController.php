17f82cad6940589da5ab64829bbb0494d77f2648
sumanahmed <suman777333@gmail.com>
2020-09-28 13:55:52 +0600

    method added to show ambulance, package & schedule rides

--- app/Http/Controllers/Web/RideController.php
+++ app/Http/Controllers/Web/RideController.php
@@ -13,4 +13,22 @@ class RideController extends Controller
         $rides = Ride::where('ride_type', 1)->get();
         return view('quicar.backend.ride.current_ride', compact('rides'));
     }
+
+    //show current rides
+    public function scheduleRide(){
+        $rides = Ride::where('ride_type', 2)->get();
+        return view('quicar.backend.ride.schedule_ride', compact('rides'));
+    }
+
+    //show current rides
+    public function ambulanceRide(){
+        $rides = Ride::where('ride_type', 3)->get();
+        return view('quicar.backend.ride.ambulance_ride', compact('rides'));
+    }
+
+    //show current rides
+    public function packageRide(){
+        $rides = Ride::where('ride_type', 4)->get();
+        return view('quicar.backend.ride.package_ride', compact('rides'));
+    }
 }
