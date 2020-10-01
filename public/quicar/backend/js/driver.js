4d8b5507d193aeb1733093ff3fa907d9e8693da0
sumanahmed <suman777333@gmail.com>
2020-10-01 22:54:40 +0600

    method added for driver update & destroy

--- public/quicar/backend/js/driver.js
+++ public/quicar/backend/js/driver.js
@@ -7,7 +7,7 @@ $('#driverTable').DataTable({
     }
 });
 
-//create offer
+//create driver
 $("#createDriver").click(function (e) {
     e.preventDefault();
 
@@ -74,13 +74,24 @@ $("#createDriver").click(function (e) {
                 }               
             }else{
                 $('#createDriverModal').modal('hide');
+                if(response.data.account_status == 1){
+                    var status = "Active";
+                }else{
+                    var status = "Inactive";
+                }
                 $("#allDriver").append('' +
                     '<tr class="driver-'+ response.data.id +'">\n' +
                         '<td>'+ response.data.name +'</td>\n' +
                         '<td>'+ response.data.email +'</td>\n' +
                         '<td>'+ response.data.phone +'</td>\n' +
+                        '<td><img src="http://quicarbd.com/'+ response.data.img +'" style="width:80px;height:80px;"/></td>\n' +
+                        '<td>'+ status +'</td>\n' +
                         '<td style="vertical-align: middle;text-align: center;">\n' +                        
-                            '<button class="btn btn-info" data-toggle="modal" id="editDriver" data-target="#editDriverModal" data-id="'+ response.data.id +'" data-name="'+ response.data.value +'" title="Edit"><i class="fas fa-edit"></i></button>\n' +                            
+                            '<button class="btn btn-info" data-toggle="modal" id="editDriver" data-target="#editDriverModal" data-id="'+ response.data.id +'" data-name="'+ response.data.name +'"\n' + 
+                                'data-email="'+ response.data.email +'" data-phone="'+ response.data.phone +'" data-dob="'+ response.data.dob +'" data-owner_id="'+ response.data.owner_id +'" data-nid="'+ response.data.nid +'"\n' + 
+                                'data-division="'+ response.data.division +'" data-district="'+ response.data.district +'" data-address="'+ response.data.address +'" data-img="http://quicarbd.com/'+ response.data.img +'"\n' + 
+                                'data-license="http://quicarbd.com/'+ response.data.license +'" title="Edit"><i class="fas fa-edit"></i></button>\n' +                            
+                            '<button class="btn btn-danger" data-toggle="modal" id="deleteDriver" data-target="#deleteDriverModal" data-id="'+ response.data.id +'" title="Delete"><i class="fas fa-trash"></i></button>\n' +
                         '</td>\n' +
                     '</tr>'+
                 '');
@@ -99,7 +110,7 @@ $("#createDriver").click(function (e) {
     });
 });
 
-//open edit Color modal
+//open edit Driver modal
 $(document).on('click', '#editDriver', function () {
     console.log('yess');
     $('#editDriverModal').modal('show');
@@ -113,6 +124,130 @@ $(document).on('click', '#editDriver', function () {
     $('#edit_division').val($(this).data('division'));
     $('#edit_district').val($(this).data('district'));
     $('#edit_address').val($(this).data('address'));
-    $("#previous_image").attr("src", 'http://quicarbd.com/'+'img');
-    $("#previous_license").attr("src", 'http://quicarbd.com/'+'license');
+    $("#previous_image").attr("src", $(this).data('img'));
+    $("#previous_license").attr("src", $(this).data('license'));
 });
+
+//update driver
+$("#updateDriver").click(function (e) {
+    e.preventDefault();
+
+    var form_data = new FormData($("#editDriverForm")[0]);
+    form_data.append('id', $("#edit_id").val());
+    form_data.append('name', $("#edit_name").val());
+    form_data.append('email', $("#edit_email").val());
+    form_data.append('phone', $("#edit_phone").val());
+    form_data.append('dob', $("#edit_dob").val());
+    form_data.append('owner_id', $("#edit_owner_id :selected").val());
+    form_data.append('nid', $("#edit_nid").val());
+    form_data.append('division', $("#edit_division").val());
+    form_data.append('district', $("#edit_district").val());
+    form_data.append('address', $("#edit_address").val());
+
+    $.ajax({
+        type:'POST',
+        url: '/admin/drivers/update',
+        headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') },
+        enctype: 'multipart/form-data',
+        processData: false,
+        contentType: false,
+        data: form_data,
+        success:function(response){
+            if((response.errors)){
+                if(response.errors.name){
+                    $('.nameError').text(response.errors.name);
+                }else{
+                    $('.nameError').text('');
+                }               
+                if(response.errors.phone){
+                    $('.phoneError').text(response.errors.phone);
+                }else{
+                    $('.phoneError').text('');
+                }               
+                if(response.errors.dob){
+                    $('.dobError').text(response.errors.dob);
+                }else{
+                    $('.dobError').text('');
+                }               
+                if(response.errors.owner_id){
+                    $('.ownerError').text(response.errors.owner_id);
+                }else{
+                    $('.ownerError').text('');
+                }               
+                if(response.errors.nid){
+                    $('.nidError').text(response.errors.nid);
+                }else{
+                    $('.nidError').text('');
+                }               
+                if(response.errors.division){
+                    $('.divisionError').text(response.errors.division);
+                }else{
+                    $('.divisionError').text('');
+                }               
+                if(response.errors.district){
+                    $('.districtError').text(response.errors.district);
+                }else{
+                    $('.districtError').text('');
+                }               
+                if(response.errors.address){
+                    $('.addressError').text(response.errors.address);
+                }else{
+                    $('.addressError').text('');
+                }               
+            }else{
+                $('#editDriverModal').modal('hide');
+                if(response.data.account_status == 1){
+                    var status = "Active";
+                }else{
+                    var status = "Inactive";
+                }
+                $("tr.driver-"+ response.data.id).replaceWith('' +
+                    '<tr class="driver-'+ response.data.id +'">\n' +
+                        '<td>'+ response.data.name +'</td>\n' +
+                        '<td>'+ response.data.email +'</td>\n' +
+                        '<td>'+ response.data.phone +'</td>\n' +
+                        '<td><img src="http://quicarbd.com/'+ response.data.img +'" style="width:80px;height:80px;"/></td>\n' +
+                        '<td>'+ status +'</td>\n' +
+                        '<td style="vertical-align: middle;text-align: center;">\n' +                        
+                            '<button class="btn btn-info" data-toggle="modal" id="editDriver" data-target="#editDriverModal" data-id="'+ response.data.id +'" data-name="'+ response.data.name +'"\n' + 
+                                'data-email="'+ response.data.email +'" data-phone="'+ response.data.phone +'" data-dob="'+ response.data.dob +'" data-owner_id="'+ response.data.owner_id +'" data-nid="'+ response.data.nid +'"\n' + 
+                                'data-division="'+ response.data.division +'" data-district="'+ response.data.district +'" data-address="'+ response.data.address +'" data-img="http://quicarbd.com/'+ response.data.img +'"\n' + 
+                                'data-license="http://quicarbd.com/'+ response.data.license +'" title="Edit"><i class="fas fa-edit"></i></button>\n' +                            
+                            '<button class="btn btn-danger" data-toggle="modal" id="deleteDriver" data-target="#deleteDriverModal" data-id="'+ response.data.id +'" title="Delete"><i class="fas fa-trash"></i></button>\n' +
+                        '</td>\n' +
+                    '</tr>'+
+                '');
+                toastr.success('Driver Update.')
+            }
+        }
+    });
+});
+
+//open delete driver modal
+$(document).on('click', '#deleteDriver', function () {
+    $('#deleteDriverModal').modal('show');
+    $('input[name=del_id]').val($(this).data('id'));
+ });
+
+//destroy driver
+$("#destroyDriver").click(function(){
+    $.ajax({
+        type: 'POST',
+        url: '/admin/drivers/destroy',
+        headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') },
+        data: {
+            id: $('input[name=del_id]').val()
+        },
+        success: function (response) {
+            $('#deleteDriverModal').modal('hide');
+            $('.driver-' + $('input[name=del_id]').val()).remove();
+            toastr.success(response.message)
+        }
+    });
+});
+
+
+
+
+
+
