38260f2ad68cc08a95414f215bfdbeb674541d4f
sumanahmed <suman777333@gmail.com>
2020-10-04 23:09:16 +0600

    method added for push notification send

--- public/quicar/backend/js/user.js
+++ public/quicar/backend/js/user.js
@@ -11,25 +11,44 @@ $('#userTable').DataTable({
 $(document).on('click', '#userSendNotification', function () {
     var image = image_base_path + $(this).data('image');
     $('#userSendNotificationModal').modal('show');
-    $('#notification_id').val($(this).data('id'));
+    $('#n_key').val($(this).data('n_key'));
+    $('#phone').val($(this).data('phone'));
  });
 
  //destroy master category
 $("#userNotificationSend").click(function(){
+    var n_key       = $('#n_key').val();
+    var title       = $('#title').val();
+    var message     = $('#message').val();
+    var notification= $('input[name=notification]:checked').val();
+    var phone       = $('#phone').val();
     $.ajax({
         type: 'POST',
         url: '/admin/users/notification/send',
         headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') },
         data: {
-            id: $('input[name=del_id]').val()
+            n_key       : n_key,
+            title       : title,
+            message     : message,
+            notification: notification,
+            phone       : phone,
         },
         success: function (response) {
-            $('#userSendNotificationModal').modal('hide');
-            if(response.status == '403'){
-                toastr.error(response.message)
-            }else{                
+            if((response.errors)){
+                if(response.errors.title){
+                    $('.errorTitle').text(response.errors.title);
+                }else{
+                    $('.errorTitle').text('');
+                }  
+                if(response.errors.message){
+                    $('.errorMessage').text(response.errors.message);
+                }else{
+                    $('.errorMessage').text('');
+                }  
+            }else{
+                $('#userSendNotificationModal').modal('hide');
                 toastr.success(response.message)
-            }  
+            }
         }
     });
 });
