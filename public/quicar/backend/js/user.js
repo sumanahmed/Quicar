214b4f5a320360ace262ba3bd2c1676c452bf8c2
sumanahmed <suman777333@gmail.com>
2020-10-04 08:30:32 +0600

    method added user notification send

--- public/quicar/backend/js/user.js
+++ public/quicar/backend/js/user.js
@@ -6,3 +6,30 @@ $('#userTable').DataTable({
         lengthMenu: '_MENU_ items/page',
     }
 });
+
+//open send notification modal
+$(document).on('click', '#userSendNotification', function () {
+    var image = image_base_path + $(this).data('image');
+    $('#userSendNotificationModal').modal('show');
+    $('#notification_id').val($(this).data('id'));
+ });
+
+ //destroy master category
+$("#userNotificationSend").click(function(){
+    $.ajax({
+        type: 'POST',
+        url: '/admin/users/notification/send',
+        headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') },
+        data: {
+            id: $('input[name=del_id]').val()
+        },
+        success: function (response) {
+            $('#userSendNotificationModal').modal('hide');
+            if(response.status == '403'){
+                toastr.error(response.message)
+            }else{                
+                toastr.success(response.message)
+            }  
+        }
+    });
+});
