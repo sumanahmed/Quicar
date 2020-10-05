4bff668f0ca82afc22aa52aa73161061fd17735b
sumanahmed <suman777333@gmail.com>
2020-10-05 21:04:42 +0600

    method added for owner push notification & sms send

--- app/Http/Controllers/Web/OwnerController.php
+++ app/Http/Controllers/Web/OwnerController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Model\Owner;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Response;
+use Validator;
 
 class OwnerController extends Controller
 {
@@ -24,5 +27,49 @@ class OwnerController extends Controller
         }else{
             return redirect()->back()->with('error_message', 'Sorry something went wrong');
         }
+    }
+
+    //notification send
+    public function notificationSend(Request $request){         
+        $validators=Validator::make($request->all(),[
+            'title'   => 'required',
+            'message' => 'required',
+            'notification' => 'required',
+        ]);
+        if($validators->fails()){
+            return Response::json(['errors'=>$validators->getMessageBag()->toArray()]);
+        }else{ 
+            if($request->notification == 1){ 
+                //push notification send            
+                    $id      = $request->n_key;
+                    $title   = $request->title;
+                    $body    = $request->message;
+                    $client  = new Client();
+                    $client->request("GET", "http://quicarbd.com//mobileapi/general/notification/send.php?id=".$id."&title=".$title ."&body=".$body);
+                //push notification send end
+                return Response::json([
+                    'status'    => 200,
+                    'message'   => "Notification send successfully",
+                ]);
+            }else{
+                //push notification send            
+                    $id      = $request->n_key;
+                    $title   = $request->title;
+                    $body    = $request->message;
+                    $client  = new Client();
+                    $client->request("GET", "http://quicarbd.com//mobileapi/general/notification/send.php?id=".$id."&title=".$title ."&body=".$body);
+                //push notification send end
+
+                //message send
+                    $msg    = $request->message;
+                    $client = new Client();            
+                    $sms    = $client->request("GET", "http://66.45.237.70/api.php?username=01670168919&password=TVZMBN3D&number=". $request->phone ."&message=".$msg);
+                //message send end
+                return Response::json([
+                    'status'    => 200,
+                    'message'   => "Notification & SMS send successfully",
+                ]);
+            }            
+        }        
     }
 }
