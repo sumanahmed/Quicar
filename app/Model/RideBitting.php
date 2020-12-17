4e12af366baf3fbab0378fe3c7714a3c7db3e5e7
sumanahmed <suman777333@gmail.com>
2020-12-17 08:24:44 +0600

    eloquent relation create with partner, car

--- app/Model/RideBitting.php
+++ app/Model/RideBitting.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class RideBitting extends Model
 {
     protected $table = "ride_biting";
+
+    public function owner() {
+        return $this->belongsTo('App\Model\Owner','owner_id');
+    }
+
+    public function ride() {
+        return $this->belongsTo('App\Model\RideList','ride_id');
+    }
+
+    public function driver() {
+        return $this->belongsTo('App\Model\Driver','driver_id');
+    }
+
+    public function car() {
+        return $this->belongsTo('App\Model\Car','car_id');
+    }
 }
