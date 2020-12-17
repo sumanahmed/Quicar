bd088a76d3d53da0eb32248348ca8cb15a618fa9
sumanahmed <suman777333@gmail.com>
2020-12-17 08:25:15 +0600

    method added to show ride bitting with ride_id

--- app/Http/Controllers/Web/RideController.php
+++ app/Http/Controllers/Web/RideController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\Ride;
+use App\Model\RideBitting;
 use App\Model\RideList;
 
 class RideController extends Controller
@@ -13,6 +14,22 @@ class RideController extends Controller
     public function currentRide(){
         $rides = RideList::where('status','!=', 2)->where('status','!=',5)->get();
         return view('quicar.backend.ride.current_ride', compact('rides'));
+    }
+
+    //show ride bitting
+    public function rideBitting($ride_id) {
+        $ride_bittings = RideBitting::where('ride_id',$ride_id)->get();
+        return view('quicar.backend.ride.ride_bitting', compact('ride_bittings'));
+    }
+
+    //bidding Destroy
+    public function bittingDestroy($bitting_id) {
+        $ride_bitting = RideBitting::where('id',$bitting_id);
+        if($ride_bitting->delete()){
+            return redirect()->back()->with('message','Bitting deleted successfully');
+        }else{
+            return redirect()->back()->with('error_message','Sorry, something went wrong');
+        }
     }
 
     //show current ride details
